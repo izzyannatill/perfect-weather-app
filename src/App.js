2946223800ad566af942e3fca033e72be7c6bf34
izzyannatill <izzyannatill@gmail.com>
2025-03-25 20:36:13 +0000

    Updated link to open in new window

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <h1>
      Weather App
      </h1>
-    <p>The perfect weather app via<a href="https://github.com/izzyannatill/perfect-weather-app">Github</a>.</p>
+    <p>The perfect weather app by Izzy Till via<a href="https://github.com/izzyannatill/perfect-weather-app" target="blank">Github</a>.</p>
     </div>
   );
 }

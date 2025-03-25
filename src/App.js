3f4ec9a3de8bd511c9d45f2562793c828468b649
izzyannatill <izzyannatill@gmail.com>
2025-03-25 20:35:27 +0000

    Added link to the github repository

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <h1>
      Weather App
+     </h1>
+    <p>The perfect weather app via<a href="https://github.com/izzyannatill/perfect-weather-app">Github</a>.</p>
     </div>
   );
 }

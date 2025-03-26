9c6f4d70c5caaa5f51993e8a0d896a8e35811e2f
izzyannatill <izzyannatill@gmail.com>
2025-03-26 18:26:37 +0000

    Created the HTML for the weather app and a CSS file for styling.
    Also installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>
-     Weather App
-     </h1>
-    <p>The perfect weather app by Izzy Till via <a href="https://github.com/izzyannatill/perfect-weather-app" target="blank">Github</a>.</p>
+      <div className='container'>
+     <Weather/>
+     <footer>
+      <p>The perfect weather app by Izzy Till via <a href="https://github.com/izzyannatill/perfect-weather-app" target="blank">Github</a>.
+     </p>
+     </footer>
+    </div>
     </div>
   );
 }
